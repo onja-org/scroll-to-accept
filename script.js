5a15f0124fcc39d19567b365dd5d8626da8a5b76
Pinois <>
2020-07-16 14:54:24 +0300

    added new solution

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('it works');
+const terms = document.querySelector('.terms-and-conditions');
+const button = document.querySelector('.accept');
+
+const obCallback = payload => {
+	// when the HR is 100% (1) on the screen, remove the disable attribute
+	if (payload[0].intersectionRatio === 1) {
+		// make the accept button clickable!
+		button.disabled = false;
+		ob.unobserve(terms.lastElementChild);
+	}
+};
+
+// we create the observer, and give it an action
+const ob = new IntersectionObserver(obCallback, {
+	root: terms,
+	threshold: 1,
+});
+
+// we say to him/her, if you see the terms.lastElementChild (hr), execture the action/function
+ob.observe(terms.lastElementChild);
